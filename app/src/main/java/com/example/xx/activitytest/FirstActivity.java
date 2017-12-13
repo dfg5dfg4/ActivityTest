25a3902e0f4e07e4052f0076bd72b3603c26c96c
dfg5dfg4 <1536393150@qq.com>
2017-12-13 23:48:58 +0800

    First commit.

---
+++ app/src/main/java/com/example/xx/activitytest/FirstActivity.java
@@ -0,0 +1,16 @@
+package com.example.xx.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+
+    }
+}
